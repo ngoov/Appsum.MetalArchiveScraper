eff96275a24dbc4fda01a2138cc3b9fc2df5a73f
Nils Goovaerts <nils.goovaerts@egssis.com>
2022-03-14 11:01:07 +0100

    Initial commit

---
+++ Scraper/Scraper.cs
@@ -0,0 +1,9 @@
+﻿namespace Scraper;
+
+public class Scraper
+{
+    /*
+     * Use AngleSharp (https://anglesharp.github.io/)
+     * Scrape the bottom part list https://metalstorm.net/events/new_releases.php#142331
+     */
+}
